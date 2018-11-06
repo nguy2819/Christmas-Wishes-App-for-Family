703dcd72d7adaf2bfafb38be5918c4d66abec3ac
Tien Borland <nguy2819@crk.umn.edu>
2018-11-06 10:15:47 -0700

    finished up navbar with Sign Up, Log In, New Christmas Wish, Log Out, Username Icon, and Notifications in Dashboard components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
 
 const Navbar = () => {
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">Christmas Wishes App</Link>
+                <SignedInLinks />
+                <SignedOutLinks />
             </div>
         </nav>
     )
