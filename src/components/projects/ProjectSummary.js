f497d2a1ba152132c8011ec85863e70bc767ea1f
Tien Borland <nguy2819@crk.umn.edu>
2018-11-14 12:41:05 -0700

    passed the project.title from DB to the front

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return(
         <div className="project-list section">
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Christmas Wishes Title</span>
+                    <span className="card-title">{project.title}</span>
                     <p>Posted by Tien Borland</p>
                     <p className="grey-text">November 11th, 2018, 10am</p>
                 </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'Tien Borland', christmaswishes: 'successful'},
+        {id: '2', title: 'David Borland', christmaswishes: 'health'},
+        {id: '3', title: 'Eliana Borland', christmaswishes: 'happiness'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
