40160f380e8bbd606117ce14429d2170e2d3e386
Tien Borland <nguy2819@crk.umn.edu>
2018-11-19 14:03:35 -0700

    worked on Log User In and Out

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,8 @@ const ProjectSummary = ({project}) => {
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">{project.title}</span>
-                    <p>Posted by Tien Borland</p>
+                    <p>{project.christmaswishes}</p>
+                    <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
                     <p className="grey-text">November 11th, 2018, 10am</p>
                 </div>
             </div>
