7480010f052002e03cd7830e5d17b167fdbde050
Tien Borland <nguy2819@crk.umn.edu>
2018-11-21 11:05:43 -0700

    Linked between Frontend & DB Notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import {Redirect} from 'react-router-dom';
 
 class Dashboard extends Component {
     render(){
-        const {projects, auth} = this.props;
+        const {projects, auth, notifications} = this.props;
         if(!auth.uid) return <Redirect to='/signin' />
         
         return(
@@ -18,7 +18,7 @@ class Dashboard extends Component {
                         <ProjectList projects={projects}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notification />
+                        <Notification notifications={notifications}/>
                     </div>
                 </div>
             </div>
@@ -29,7 +29,8 @@ class Dashboard extends Component {
 const mapStateToProps =(state) => {
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 
