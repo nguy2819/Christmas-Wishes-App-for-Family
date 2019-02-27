ddcd2a9671f0cf3d2586f3769fa2e7dd736a7b5f
Tien Borland <nguy2819@crk.umn.edu>
2019-02-27 11:12:25 -0700

    Firebase deploy
    npm run build => move all the field from build folder over to dist folder and type "firebase deploy" in terminal

---
+++ dist/precache-manifest.eacc5b11e6d0175e539615d7b73b3a15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "68eaabf605026a0a2d83",
+    "url": "/static/js/main.68eaabf6.chunk.js"
+  },
+  {
+    "revision": "4cff3d133696dbabf4e9",
+    "url": "/static/js/1.4cff3d13.chunk.js"
+  },
+  {
+    "revision": "68eaabf605026a0a2d83",
+    "url": "/static/css/main.2ed3abfa.chunk.css"
+  },
+  {
+    "revision": "15eddc2a541909f86571221542e8c1bc",
+    "url": "/index.html"
+  }
+];
